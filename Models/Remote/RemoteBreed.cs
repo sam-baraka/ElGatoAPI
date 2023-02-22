d4fca36ac6dafc9dc8f832d2e1ffa38defa5feb1
Samuel Baraka <sababuvercetti@gmail.com>
2023-02-22 12:04:42 +0300

    feat: added more fields to the remote breed model

--- Models/Remote/RemoteBreed.cs
+++ Models/Remote/RemoteBreed.cs
@@ -14,6 +14,32 @@ namespace ElGatoAPI.Models
         public String? BreedDescription { get; set; }
         [JsonProperty("wikipedia_url")]
         public String? WikipediaUrl { get; set; }
+        [JsonProperty("cfa_url")]
+        public String? CFAUrl { get; set; }
+        [JsonProperty("vetstreet_url")]
+        public String? VetstreetUrl { get; set; }
+        [JsonProperty("vcahospitals_url")]
+        public String? VCAHospitalsUrl { get; set; }
+        [JsonProperty("origin")]
+        public String? Origin { get; set; }
+        [JsonProperty("temperament")]
+        public String? Temperament { get; set; }
+
+        [JsonProperty("life_span")]
+        public String? LifeSpan { get; set; }
+
+        [JsonProperty("indoor")]
+        public int? Indoor { get; set; }
+
+        [JsonProperty("lap")]
+        public int? Lap { get; set; }
+
+        [JsonProperty("country_code")]
+        public String? CountryCode { get; set; }
+
+        [JsonProperty("country_codes")]
+
+        public String? CountryCodes { get; set; }
 
     }
 
