bc26717965b323b89a17c1d5ebff59e5f6f3f189
Samuel Baraka <sababuvercetti@gmail.com>
2023-02-22 17:33:42 +0300

    feat: added more fields

--- Models/Breed.cs
+++ Models/Breed.cs
@@ -11,8 +11,31 @@ namespace ElGatoAPI.Models
 
             [BsonElement("Name")]
             public string BreedName { get; set; } = null!;
-        [BsonElement("Description")]
+            [BsonElement("Description")]
             public string BreedDescription { get; set; } = null!;
+            [BsonElement("WikipediaUrl")]
+            public string? WikipediaUrl { get; set; } = null!;
+            [BsonElement("CFAUrl")]
+            public string? CFAUrl { get; set; } = null!;
+            [BsonElement("VetstreetUrl")]
+            public string? VetstreetUrl { get; set; } = null!;
+            [BsonElement("VCAHospitalsUrl")]
+            public string? VCAHospitalsUrl { get; set; } = null!;
+            [BsonElement("Origin")]
+            public string? Origin { get; set; } = null!;
+            [BsonElement("Temperament")]
+            public string? Temperament { get; set; } = null!;
+            [BsonElement("LifeSpan")]
+            public string? LifeSpan { get; set; } = null!;
+            [BsonElement("Indoor")]
+            public int? Indoor { get; set; } = null!;
+            [BsonElement("Lap")]
+            public int? Lap { get; set; } = null!;
+            [BsonElement("CountryCode")]
+            public string? CountryCode { get; set; } = null!;
+            [BsonElement("CountryCodes")]
+            public string? CountryCodes { get; set; } = null!;
+
         
     }
 }
