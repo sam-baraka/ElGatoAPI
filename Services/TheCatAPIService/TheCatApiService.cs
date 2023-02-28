e369915450e606532ca3fc5940ca7870a7b932c7
Samuel Baraka <sababuvercetti@gmail.com>
2023-02-28 18:51:00 +0300

    feat: Added query params

--- Services/TheCatAPIService/TheCatApiService.cs
+++ Services/TheCatAPIService/TheCatApiService.cs
@@ -8,14 +8,17 @@ namespace ElGatoAPI.Services
      ApiService apiService = new ApiService();
         public async Task<List<RemoteBreed>?> GetBreeds()
         {   
-            var response= await ApiService.GetData("breeds");
+            var response= await ApiService.GetData(path:"breeds");
             var breeds = JsonConvert.DeserializeObject<List<RemoteBreed>>(response);
             return breeds;
         }
 
         public async Task<List<RemoteImage>?> GetImages()
         {
-            var response = await ApiService.GetData("images/search");
+            var response = await ApiService.GetData(path:"images/search",parameters: new Dictionary<string, string>()
+            {
+                {"limit", "100"},
+            });
             var images = JsonConvert.DeserializeObject<List<RemoteImage>>(response);
             return images;
         }
